b58f2a6c153f18f4c8ac8959e6dbbeae444c5937
yutik-cs <yutikpatel.cs@gmail.com>
2024-06-28 10:52:52 +0530

    Radio button files

---
+++ RadioButton.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RadioButton.aspx.cs" Inherits="WebApplication1.RadioButton" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+
+            <asp:Label ID="gender" runat="server" Text="Gender"></asp:Label>
+            <asp:RadioButton ID="male" GroupName="gen" runat="server" Text="Male" />
+            <asp:RadioButton ID="female" GroupName="gen" runat="server"  Text="Female" />
+            <asp:Label ID="select" runat="server" Text=""></asp:Label>
+            <asp:Button ID="Button1" runat="server" Text="Check" />
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ RadioButton.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class RadioButton : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (male.Checked) Response.Write("Male checked");
+            if (female.Checked) Response.Write("Female checked");
+        }
+
+        /*protected void male_CheckedChanged(object sender, EventArgs e)
+        {
+            Response.Write("Male checked");
+        }
+
+      
+
+        protected void female_CheckedChanged(object sender, EventArgs e)
+        {
+            Response.Write("Female checked");
+        }*/
+    }
+}
