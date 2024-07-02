7c04f66c1e26452c4d07c49183abffe7ffb5c2ac
Yutik Patel <126074693+yutik-cs@users.noreply.github.com>
2024-07-02 11:34:21 +0530

    Add files via upload

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MySite
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            FileUpload1.SaveAs(Server.MapPath("~/files/"+FileUpload1.FileName));
+        }
+    }
+}
