efe3a1ee6d2713a5ff85f1d06fe8224da90ac78d
Yutik Patel <126074693+yutik-cs@users.noreply.github.com>
2024-09-03 06:09:19 +0530

    Add files via upload

---
+++ WebForm9.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm9.aspx.cs" Inherits="Prac10.WebForm9" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label runat="server" ID="L1" Text="Username" />
+            <asp:TextBox runat="server" ID="username" Text=""/>
+            <br />
+            <asp:Button ID="submit" runat="server" Text="Submit" OnClick="submit_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm9.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Prac10
+{
+    public partial class WebForm9 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                HttpCookie cookie = Request.Cookies["user"];
+                if (cookie != null)
+                {
+                    L1.Text = "Welcome back " + cookie.Value;
+                }
+            }
+        }
+
+        protected void submit_Click(object sender, EventArgs e)
+        {
+            String un = username.Text;
+            Response.Write(un);
+            if (!String.IsNullOrEmpty(un))
+            {
+                HttpCookie cookie = new HttpCookie("user");
+                cookie.Value = un;
+                cookie.Expires = DateTime.Now.AddMonths(6);
+                Response.Cookies.Add(cookie);
+                L1.Text = "Name saved! Welcome, " + cookie.Value + ".";
+            }
+            else
+            {
+                L1.Text = "Please enter your name";
+            }
+        }
+    }
+}
